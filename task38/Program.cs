d7ef4efb3c272004b6e6ece60a1971c8acbecb65
Natalya <alkinatalya@yandex.com>
2022-11-19 19:38:28 +0300

    add task38

---
+++ task38/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+double[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    double[] answer = new double[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom) + rnd.NextDouble();
+    }
+    return answer;
+}
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+double DifferenceMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    double difference = max - min;
+return difference;
+}
+double[] array = GenerateArray(4, 100, 1000);
+PrintArray(array);
+System.Console.WriteLine($" Разница между максимальным и минимальным элементом массива:{DifferenceMaxMin(array)}");
